3dd8f7cb0131acbfbe69b4936575c3f6523efdd6
aditihaiman <ahaiman00@stuy.edu>
2019-02-13 09:31:35 -0500

    toString

--- KnightBoard.java
+++ KnightBoard.java
@@ -3,8 +3,12 @@ import java.*;
 public class KnightBoard{
 
   private int[][] board;
+  private int rows;
+  private int cols;
 
-  public KnightBoard(int rows, int cols) {
+  public KnightBoard(int row, int col) {
+    rows = row;
+    cols = col;
     board = new int[rows][cols];
     for (int x = 0; x < rows; x++) {
       for (int y = 0; y < cols; y++) {
@@ -13,7 +17,17 @@ public class KnightBoard{
     }
   }
 
-  public String toString() {}
+  public String toString() {
+    String output = "";
+    for (int x = 0; x < rows; x++) {
+      for (int y = 0; y < cols; y++) {
+        if (board[x][y] / 10 == 0) output+= (" " + board[x][y]);
+        else output += board[x][y];
+      }
+      output+="\n";
+    }
+    return output;
+  }
 
   public boolean solve(int row, int col) {}
 
